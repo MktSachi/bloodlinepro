9fe3ec6c317b3b6738d8e3e05cdfdb8158b0515f
MandiraPerera <mandiraseshani2001@gmail.com>
2024-05-31 22:04:57 +0530

    add about page and vision & mission page

---
+++ About/about.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>About BloodLinePro</title>
+        <style>
+            body{
+                font-family: Arial, sans-serif;
+                background-color: #FEF5E7 ;
+            }
+            h1{
+                background-color: #831A1A;
+                color: white;
+            }
+            img {
+              display: block;
+              margin-left: auto;
+              margin-right: auto;
+            }
+            p{
+                margin-left: 10%;
+                margin-right: 10%;
+            }
+            img{
+                border-radius: 10%;
+            }
+        </style>
+    </head>
+    <body>
+        
+        <h1 align="center">About BloodLinePro</h1>
+        <p align="justify">Welcome to BloodLinePro, the ultimate solution for revolutionizing blood bank management. Our state-of-the-art system is designed to streamline 
+           every aspect of blood donation, inventory control, and request processing. With BloodLinePro, you can count on continuous operation, especially 
+           during emergencies, ensuring that real-time blood requests are handled seamlessly and efficiently.<br><br>
+
+           BloodLinePro stands out with its exceptional performance, offering lightning-fast response times that empower healthcare professionals to make 
+           swift, informed decisions. Our user-friendly interface guarantees a smooth experience, featuring intuitive navigation and clear error messages, 
+           making it easy for both donors and healthcare providers to interact with the system.<br><br>
+
+           Flexibility is at the core of BloodLinePro. Our system easily integrates new features and scales effortlessly to accommodate a growing user base 
+           and expanding healthcare facilities, all while maintaining peak performance. This adaptability ensures that BloodLinePro stays ahead of the curve, 
+           evolving with the latest advancements and user needs.<br><br>
+
+           Security is paramount at BloodLinePro. We employ advanced data encryption and robust user authentication to protect sensitive donor information, 
+           ensuring privacy and maintaining the trust of our users. Your data's safety is our top priority.<br><br>
+
+           Join us in transforming the landscape of blood donation and management. BloodLinePro is dedicated to making the process more efficient, secure, 
+           and user-friendly. Together, we can save lives, one donation at a time.</p><br><br>
+        
+        <img width="20%" src="image4.jpeg" alt="donating blood"/>
+        
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
