c95a87704e13fb912dcea009993eda48beb0c049
MandiraPerera <mandiraseshani2001@gmail.com>
2024-06-02 18:33:11 +0530

    add about page and vision & mission page

---
+++ vision&mission/vision&mission.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Vision & Mission</title>
+        <style>
+            body{
+                font-family: Arial, sans-serif;
+                background-color: #FEF5E7;
+            }
+            h1{
+                background-color: #831A1A;
+                color: white;
+            }
+            div.box{
+                width: 75%;
+                border: 5px solid #831A1A;
+                border-radius: 20px;
+                background-color: white;
+            }
+            h2{
+                color: #831A1A;
+            }
+        </style>
+    </head>
+    <body>
+        <h1 align="center">Vision & Mission</h1>
+        
+        <div align="center">
+        <div class="box">
+        <h2>අපගේ දැක්ම</h2>
+        <p>ලොවටම ආදර්ශමත් වූ ගුණාත්මක බවින් පිරිපුන් ජාතික රුධිර සේවාවක් සුරක්ෂිත කිරීම.</p>
+        
+        <h2>எமது நோக்கு</h2>
+        <p>உலகிற்கே ஈடிணையற்ற முன்மாதிரியான தரம்மிக்க பரிபூரண குருதிச் சேவையைப் பேணிப்பாதுகாத்தல்.</p>
+        
+        <h2>Our Vision</h2>
+        <p>To be a unique model for the world securing Quality Assured Blood Services, through a nationally coordinated system.</p>
+        </div>
+        
+        <br><br>
+        
+        <div class="box">
+        <h2>අපගේ මෙහෙවර</h2>
+        <p>ලෝක සෞඛ්‍ය සංවිධානයේ නිර්දේශයන්ට හා ජාතික අවශ්‍යතාවයන්ට අනුකූලව ගුණාත්මකවුත්, සුරක්ෂිතවූත්, ප්‍රමාණවත් හා ඵලදායීවූත් ජාතික රුධිර සැපයුමක් හා වෙනත් අනුබද්ධ රසායනාගාර, ප්‍රතිකාර, අධ්‍යයන හා පර්යේෂණ සේවාවන් පවත්වාගෙන යාම.</p>
+        
+        <h2>எமது பணி</h2>
+        <p>உலக சுகாதார தாபானத்தின் சிபார்சுகளுக்கும் தேசிய தேவைப்பாடுகளுக்கும் அமைய தரமானதும், பாதுகாப்பானதும், போதுமான அளவு பயனையும் தரக்கூடிய தேசிய குருதி விநியோகமும் அத்துடன் ஏனைய இணைந்த இரசாயனகூடம், சிகிச்சை,கற்கை மற்றும் ஆராய்ச்சி போன்ற சேவைகளை மேற்கொள்ளல்.</p>
+        
+        <h2>Our Mission</h2>
+        <p>To ensure the quality, safety, adequacy and cost effectiveness of the blood supply and related laboratory, clinical, academic and research services in accordance with national requirements and WHO recommendations.</p>
+        </div>
+        </div>
+        
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
