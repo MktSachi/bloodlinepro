f1e102d75cc30ff9e9a829a1b591719ac2742e66
AhinsaAru <ahinsa1222@gmail.com>
2024-06-06 09:40:54 +0530

    Commented

--- Donate_page/Donate.php
+++ Donate_page/Donate.php
@@ -5,15 +5,16 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <body>
 
-<div class=" w3-padding-16 w3-margin-bottom w3-center" style="background-color: rgb(131,26,26); color:white;">
-  <h1 class="w3-extra-lage">Blood Donation</h1>
-</div>
+  <!--header-->
+  <div class=" w3-padding-16 w3-margin-bottom w3-center" style="background-color: rgb(131,26,26); color:white;">
+    <h1 class="w3-extra-lage">Blood Donation</h1>
+  </div>
 
-<div class="w3-row-padding w3-content" style="max-width:1400px">
-  <div class="w3-twothird">
-    
-    
-    <h2>Who can donate blood?</h2></br>
+  <div class="w3-row-padding w3-content" style="max-width:1400px">
+    <!--right side content-->
+    <div class="w3-twothird"> 
+      <!--first topic-->
+      <h2>Who can donate blood?</h2></br>
       <div class="w3-justify">
         <ul>
           <li>Age above 18 years and below 60 years.</li></br>
@@ -24,87 +25,81 @@
           <li>Free from "Risk Behaviours".</li></br>
         </ul>
 
-
+        <!--second topic - list-->
         <h2>Types Of Blood Donations</h2></br>
-    <ol>
-        <li>Whole Blood Donation:</li></br>
-        <p><b>What:</b>The most common type of blood donation where approximately one pint of whole blood is collected.</p>
-        <p><b>Uses:</b>Used for transfusions and can be separated into red cells, plasma, and platelets.</p>
-        <p><b>Frequency:</b>Every 56 days.</p></br>
+        <ol>
+            <li>Whole Blood Donation:</li></br>
+                <p><b>What:</b>The most common type of blood donation where approximately one pint of whole blood is collected.</p>
+                <p><b>Uses:</b>Used for transfusions and can be separated into red cells, plasma, and platelets.</p>
+                <p><b>Frequency:</b>Every 56 days.</p></br>
         
-        <li>Platelet Donation (Apheresis):</li></br>
-        <p><b>What:</b>Blood is drawn, platelets are separated, and the remaining blood components are returned to the donor.</p>
-        <p><b>Uses:</b>Crucial for cancer patients, organ transplant recipients, and those undergoing major surgeries.</p>
-        <p><b>Frequency:</b>Every 7 days, up to 24 times a year.</p></br>
+            <li>Platelet Donation (Apheresis):</li></br>
+                <p><b>What:</b>Blood is drawn, platelets are separated, and the remaining blood components are returned to the donor.</p>
+                <p><b>Uses:</b>Crucial for cancer patients, organ transplant recipients, and those undergoing major surgeries.</p>
+                <p><b>Frequency:</b>Every 7 days, up to 24 times a year.</p></br>
         
-        <li>Plasma Donation:</li></br>
-        <p><b>What:</b>Blood is drawn, plasma is separated, and the rest is returned to the donor.</p>
-        <p><b>Uses:</b>Treats patients with clotting disorders, burns, and shock.</p>
-        <p><b>Frequency:</b>Every 28 days, up to 13 times a year.</p></br>
+            <li>Plasma Donation:</li></br>
+                <p><b>What:</b>Blood is drawn, plasma is separated, and the rest is returned to the donor.</p>
+                <p><b>Uses:</b>Treats patients with clotting disorders, burns, and shock.</p>
+                <p><b>Frequency:</b>Every 28 days, up to 13 times a year.</p></br>
         
-        <li>Double Red Cell Donation:</li></br>
-        <p><b>What:</b>Two units of red blood cells are collected while returning plasma and platelets to the donor.</p>
-        <p><b>Uses:</b>Beneficial for trauma patients, newborns, and those with sickle cell anemia.</p>
-        <p><b>Frequency:</b>Every 112 days, up to 3 times a year.</p>    
-    </ol>
+            <li>Double Red Cell Donation:</li></br>
+                <p><b>What:</b>Two units of red blood cells are collected while returning plasma and platelets to the donor.</p>
+                <p><b>Uses:</b>Beneficial for trauma patients, newborns, and those with sickle cell anemia.</p>
+                <p><b>Frequency:</b>Every 112 days, up to 3 times a year.</p>    
+          </ol>
 
-    <div style="padding-top: 20%;">
-    <img src="image/1000_F_229689857_5RdcATAD78UwGeNtobjHd28muJkveBba.jpg" width="150%" height="50%">
+        <!--image-->
+        <div style="padding-top: 20%;">
+          <img src="image/1000_F_229689857_5RdcATAD78UwGeNtobjHd28muJkveBba.jpg" width="150%" height="50%">
+        </div>
+      </div>
+    </div>
+
+    <!--left side content-->
+    <div class="w3-third">
+
+      <!--first topic-->
+      <div class="w3-container w3-light-grey">
+        <h2>Risk Behaviours</h2>
+        <ul>
+            <li>Engaging in sex with any of the above.</li></br>
+            <li>Having more than one sexual partner</li></br>
+            <li>Sex workers and their clients.</li></br>
+            <li>Homosexuals.</li></br>
+            <li>Drug addicts.</li></br>
+        </ul>
+      </div></br>
+
+       <!--second topic-->
+      <div class="w3-container w3-light-grey w3-justify">
+        <h2>Type Of Donors</h2>
+        <ul>
+            <li>Voluntary non remunerated donors. (donate for the sake of others and 
+              do not expect any benefit. their blood is considered safe and healthy)</li></br>
+            <li>Replacement donors. (donate to replace the units used for their friends or family members)</li></br>
+            <li>Paid donors. (receive payment for donation)</li></br>
+            <li>Directed donors. (donate only for a specific patient's requirement)</li></br>
+        </ul>
+      </div></br>
+
+       <!--third topic-->
+      <div class="w3-container w3-light-grey w3-justify">
+        <h2>The Donation Process</h2>
+        <ul>
+            <li>Registration:</li>
+              <p>You will fill out a donor registration form with basic information.</p>
+            <li>Health Screening:</li>
+              <p>A brief health history and mini-physical (checking temperature, pulse, blood pressure, and hemoglobin level).</p>
+            <li>Donation:</li>
+              <p>The actual blood draw typically takes about 10 minutes for whole blood donation. Platelet and plasma donations take longer.</p>
+            <li>Recovery:</li>
+              <p>After donation, you will rest and have refreshments. It’s important to 
+                 drink plenty of fluids and avoid strenuous activities for the rest of the day</p>
+        </ul>
+      </div>
     </div>
   </div>
-
-   
-  </div>
-
-
-  <div class="w3-third">
-    <div class="w3-container w3-light-grey">
-      
-      <h2>Risk Behaviours</h2>
-    <ul>
-        <li>Engaging in sex with any of the above.</li></br>
-         <li>Having more than one sexual partner</li></br>
-        <li>Sex workers and their clients.</li></br>
-        <li>Homosexuals.</li></br>
-        <li>Drug addicts.</li></br>
-    </ul>
-
-
-    </div>
-    <br>
-    <div class="w3-container w3-light-grey w3-justify">
-      
-      <h2>Type Of Donors</h2>
-    <ul>
-        <li>Voluntary non remunerated donors. (donate for the sake of others and 
-            do not expect any benefit. their blood is considered safe and healthy)</li></br>
-        <li>Replacement donors. (donate to replace the units used for their friends or family members)</li></br>
-        <li>Paid donors. (receive payment for donation)</li></br>
-        <li>Directed donors. (donate only for a specific patient's requirement)</li></br>
-    </ul>
-
-    </div>
-    <br>
-    <div class="w3-container w3-light-grey w3-justify">
-    <h2>The Donation Process</h2>
-    <ul>
-        <li>Registration:</li>
-            <p>You will fill out a donor registration form with basic information.</p>
-        <li>Health Screening:</li>
-            <p>A brief health history and mini-physical (checking temperature, pulse, blood pressure, and hemoglobin level).</p>
-        <li>Donation:</li>
-            <p>The actual blood draw typically takes about 10 minutes for whole blood donation. Platelet and plasma donations take longer.</p>
-        <li>Recovery:</li>
-            <p>After donation, you will rest and have refreshments. It’s important to 
-               drink plenty of fluids and avoid strenuous activities for the rest of the day</p>
-      
-    </ul>
-
-    </div>
-
-   
-  </div>
-</div>
 
 
   
--- Donate_page/about_blood.php
+++ Donate_page/about_blood.php
@@ -6,23 +6,15 @@
     <link rel="stylesheet" href="css/C_about.css">
 </head>
 
-
-
 <body style="font-family:Arial; line-height: 1.5;">
-    <div class="topic">
-        <h1>About Blood</h1>
-    </div>
+
+    <!--Topic -->
+    <div class="topic"><h1>About Blood</h1></div>
     
-    <div>
-       
+    <!-- video -->
+    <div><embed src="Video/The Components of Blood and Their Importance.mp4" type="video/mp4" height="500px" width="100%" autoplay="true"></div>
 
-    <embed src="Video/The Components of Blood and Their Importance.mp4" type="video/mp4" height="500px" width="100%" autoplay="true">
-
- 
-
-    </div>
-
-  
+    <!-- history-->
     <div class="content">
         <h2>The Evolution of Blood Transfusion: A Journey Through Time</h2>
         <p>Blood is a vital fluid that circulates throughout the body, delivering essential 
@@ -43,7 +35,7 @@
         between donors and recipients. His work earned him the Nobel Prize and revolutionized
         the field of blood transfusion.</p>
   
-    
+         <!-- division areas of blood groups-->
         <div class="row">
             <div class="column">
                 <div class="image">
@@ -81,46 +73,45 @@
                 </div>
             </div>
         
-         </div>
+        </div>
     
-    <h2>The Development of Blood Services</h2>
-    <p>In 1946, the Blood Transfusion Service was established 
-        to manage and facilitate blood donations. This was followed 
-        by the formation of the National Blood Service in 1950, 
-        specifically tasked with collecting and providing blood supplies 
-        to hospitals across Sri Lanka. These organizations have played a 
-        crucial role in ensuring a steady and safe supply of blood for medical use.</p>
+        <h2>The Development of Blood Services</h2>
+        <p>In 1946, the Blood Transfusion Service was established 
+            to manage and facilitate blood donations. This was followed 
+            by the formation of the National Blood Service in 1950, 
+            specifically tasked with collecting and providing blood supplies 
+            to hospitals across Sri Lanka. These organizations have played a 
+            crucial role in ensuring a steady and safe supply of blood for medical use.</p>
     
-    <h2>Blood Components and Shelf Life</h2>
-    <p>Blood is a complex substance composed of several key components: red cells, 
-        white cells, platelets, and plasma. Each component has a specific function
-        and shelf life. For instance, red cells, essential for transporting oxygen,
-        can be stored for 35 to 42 days. Platelets, which are crucial for blood clotting,
-        have a much shorter shelf life of only five days. This limited storage time 
-        underscores the constant need for fresh blood donations.</p>
-    
-    
-  </div>
+        <h2>Blood Components and Shelf Life</h2>
+        <p>Blood is a complex substance composed of several key components: red cells, 
+            white cells, platelets, and plasma. Each component has a specific function
+            and shelf life. For instance, red cells, essential for transporting oxygen,
+            can be stored for 35 to 42 days. Platelets, which are crucial for blood clotting,
+            have a much shorter shelf life of only five days. This limited storage time 
+            underscores the constant need for fresh blood donations.</p>
+    </div>
 
-  <div class="right">
-    <h2>Blood Donation: Who Can Donate?</h2>
-    <p>Almost anyone between the ages of 18 and 55 can become a new blood donor, 
-        with regular donors able to continue until the age of 60. Blood donation 
-        is a simple yet profoundly impactful act that can save lives. Given that 
-        the average adult has about 5 trillion red blood cells per liter of blood, 
-        and approximately 25 trillion red cells in total, the human body is well-equipped 
-        to regenerate the donated blood.</p>
-  </div>
+    <!-- right content ash color-->
+    <div class="right">
+        <h2>Blood Donation: Who Can Donate?</h2>
+        <p>Almost anyone between the ages of 18 and 55 can become a new blood donor, 
+            with regular donors able to continue until the age of 60. Blood donation 
+            is a simple yet profoundly impactful act that can save lives. Given that 
+            the average adult has about 5 trillion red blood cells per liter of blood, 
+            and approximately 25 trillion red cells in total, the human body is well-equipped 
+            to regenerate the donated blood.</p>
+    </div>
     
-  <div class="right">
-    <h2>The Lifespan of Red Blood Cells</h2>
-    <p>Interestingly, the human body continuously renews its blood supply. 
-        Approximately 25 million red blood cells, or 0.1% of the total, die 
-        day. This amounts to roughly 300 red blood cells per second. The body’s 
-        remarkable ability to replenish these cells ensures that regular blood 
-        donation does not adversely affect the donor’s health.</p>
-  </div>
-</div>
+    <div class="right">
+        <h2>The Lifespan of Red Blood Cells</h2>
+        <p>Interestingly, the human body continuously renews its blood supply. 
+            Approximately 25 million red blood cells, or 0.1% of the total, die 
+            day. This amounts to roughly 300 red blood cells per second. The body’s 
+            remarkable ability to replenish these cells ensures that regular blood 
+            donation does not adversely affect the donor’s health.</p>
+    </div>
+   
 
 </body>
 </html>
