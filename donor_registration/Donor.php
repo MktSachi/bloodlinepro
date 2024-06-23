a5c7847eeddfba6315001b1665bfa859bed7e846
MktSachi <mktheekshana2001@gmail.com>
2024-06-23 12:32:16 +0530

    errors

--- donor_registration/Donor.php
+++ donor_registration/Donor.php
@@ -25,7 +25,7 @@ class Donor {
     }
 
     public function register($data, $profile_picture_path) {
-        $sql = "INSERT INTO donors (first_name, last_name, donorNIC, username, email, password, phoneNumber, address, address2, address2, gender, bloodType, hiv, heart_disease, diabetes, fits, paralysis, lung_diseases, liver_diseases, kidney_diseases, blood_diseases, cancer, other_health_conditions, profile_picture) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO donors (first_name, last_name, donorNIC, username, email, password, phoneNumber, address, address2, gender, bloodType, hiv, heart_disease, diabetes, fits, paralysis, lung_diseases, liver_diseases, kidney_diseases, blood_diseases, cancer, other_health_conditions, profile_picture) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         $stmt = $this->db->prepare($sql);
         $stmt->bind_param("sssssssssssssssssssssss", $data['firstName'], $data['lastName'], $data['donorNIC'], $data['username'], $data['email'], $data['password_hashed'], $data['phoneNumber'], $data['address'], $data['address2'], $data['gender'], $data['bloodType'], $data['hiv'], $data['heart_disease'], $data['diabetes'], $data['fits'], $data['paralysis'], $data['lung_diseases'], $data['liver_diseases'], $data['kidney_diseases'], $data['blood_diseases'], $data['cancer'], $data['otherHealthConditions'], $profile_picture_path);
         return $stmt->execute();
@@ -40,27 +40,28 @@ class Donor {
         return $result->fetch_assoc();
     }
 
+
     public function getDonorDetailsByNIC($donorNIC) {
-        $sql = "SELECT first_name, last_name, bloodType, email, phoneNumber, username, address, address2, gender FROM donors WHERE donorNIC = ?";
-        $stmt = $this->db->prepare($sql);
-        $stmt->bind_param("s", $donorNIC);
-        $stmt->execute();
-        $result = $stmt->get_result();
+    $sql = "SELECT first_name, last_name, bloodType, email, phoneNumber, username, address, address2, gender FROM donors WHERE donorNIC = ?";
+    $stmt = $this->db->prepare($sql);
+    $stmt->bind_param("s", $donorNIC);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-        if ($result->num_rows > 0) {
-            return $result->fetch_assoc();
-        } else {
-            return false;
-        }
+    if ($result->num_rows > 0) {
+        return $result->fetch_assoc();
+    } else {
+        return false;
     }
+}
 
-    public function deleteDonorByNIC($donorNIC) {
-        $sql = "DELETE FROM donors WHERE donorNIC = ?";
-        $stmt = $this->db->prepare($sql);
-        $stmt->bind_param("s", $donorNIC);
-        $result = $stmt->execute();
-        $stmt->close();
-        return $result;
-    }
+public function deleteDonorByNIC($donorNIC) {
+    $sql = "DELETE FROM donors WHERE donorNIC = ?";
+    $stmt = $this->db->prepare($sql);
+    $stmt->bind_param("s", $donorNIC);
+    $result = $stmt->execute();
+    $stmt->close();
+    return $result;
+}
 }
 ?>
