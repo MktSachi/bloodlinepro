e4e5c56936f2ab91899a632c4cd9b06e695559de
MandiraPerera <mandiraseshani2001@gmail.com>
2024-06-26 15:48:09 +0530

    add admin ui

---
+++ adminUi/dashboard.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Admin Dashboard</title>
+        <link rel="stylesheet" href="css/dashboard.css">
+    </head>
+    <body>
+
+        <div class="box">
+            <a href="createHp.php">
+                <div>
+                    <img src="Images/create.png" alt="Acc icon"/>
+                    <p>Create HP accounts</p>
+                </div>
+            </a>
+        </div>
+        <div class="box">
+            <a href="#">
+                <div>
+                    <img src="Images/hplist.png" alt="Profile icon"/>
+                    <p>HP accounts</p>
+                </div>
+            </a>
+        </div>
+        <div class="box">
+            <a href="#">
+                <div>
+                    <img src="Images/inventories.png" alt="Inventory icon"/>
+                    <p>Inventory</p>
+                </div>
+            </a>
+        </div>
+        <div class="box">
+            <a href="#">
+                <div>
+                    <img src="Images/availability.png" alt="Availability icon"/>
+                    <p>Availability</p>
+                </div>
+            </a>
+        </div>
+        <div class="box">
+            <a href="hospital.php">
+                <div>
+                    <img src="Images/hospitals.png" alt="Hospital icon"/>
+                    <p>Hospitals</p>
+                </div>
+            </a>
+        </div>
+        <div class="box">
+            <a href="#">
+                <div>
+                    <img src="Images/request.png" alt="Blood request icon"/>
+                    <p>View blood requests</p>
+                </div>
+            </a>
+        </div>
+        <div class="box">
+            <a href="#">
+                <div>
+                    <img src="Images/report.png" alt="Report icon"/>
+                    <p>Generate reports</p>
+                </div>
+            </a>
+        </div>
+
+    </body>
+</html>
