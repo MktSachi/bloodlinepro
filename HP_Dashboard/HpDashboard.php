f63d0470562fe43fe9cf327a9680c5ae9de081ef
Your Name <you@example.com>
2024-07-03 10:05:46 +0530

    Remove

--- HP_Dashboard/HpDashboard.php
+++ HP_Dashboard/HpDashboard.php
@@ -187,7 +187,7 @@
 
       <!-- Donation camps -->
       <div class="w3-quarter gap-2">
-        <a href="View/DonationCamp.php">
+        <a href="">
           <div class="w3-container">
             <div class="button-text">
               <h4>Donation Camp</h4>
