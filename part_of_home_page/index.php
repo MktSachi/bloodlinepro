c5819a69f1bc04ba3b7d8b1ab9f05d38ec09777b
MktSachi <mktheekshana2001@gmail.com>
2024-07-03 18:57:11 +0530

    modify home page add some internale css

--- part_of_home_page/index.php
+++ part_of_home_page/index.php
@@ -16,19 +16,47 @@
   <link rel="stylesheet" href="../assets/css/header.css">
   <link rel="stylesheet" href="../assets/css/footer.css">
   <link rel="stylesheet" href="../assets/css/fe.css">
+  <link rel="stylesheet" href="../assets/css/textcard.css">
   <link rel="stylesheet" href="../part_of_home_page/Slideshow/slideshow.css">
   <script type="text/javascript" src="Js/slide.js"></script>
   <title>Blood Bank Management System</title>
-  <link rel="icon" href="../Image/1212.png" type="image/icon type">
+  <link rel="icon" href="../Image/logo3 (1).png" type="image/icon type">
+  <style>body{
+    background-color: white;
+
+  
+  }
+  h1, h2{
+    font-weight: bold; /* Makes all headings bold */
+  }
+.content-wrapper {
+  margin: 60px; /* Adjust the margin as per your design needs */
+}
+
+
+
+  </style>
+  
 </head>
 <body class="p-0 m-0 border-0 bd-example">
   
   <?php include './home/header.php'; ?>
   <?php include './Slideshow/slide.php'; ?>
  <!-- <?php include './home/feature.php'; ?> -->
+ 
   <?php include './home/discription.php'; ?>
+  <div class="content-wrapper">
+  <?php include './home/text.php'; ?>
+</div>
+<div class="content-wrapper">
   <?php include './home/card.php'; ?>
+</div>
+<div class="content-wrapper">
+  <?php include './home/TextBar.php'; ?>
+</div>
+  <div class="content-wrapper">
   <?php include './home/pic.php'; ?>
+</div>
   <?php include './home/footer.php'; ?>
  
   <!-- Bootstrap Bundle JS (includes Popper) -->
