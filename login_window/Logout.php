6fc1c92fe82b9fe222159e87d878a64a857e40c7
MktSachi <mktheekshana2001@gmail.com>
2024-08-02 23:04:22 +0530

    destroy cookies

--- login_window/Logout.php
+++ login_window/Logout.php
@@ -1,23 +1,20 @@
 <?php
 session_start();
 
-
 if (!isset($_POST['confirm_logout'])) {
     header("Location: ConfirmLogout.php");
     exit();
 }
 
+// Set session destroy time to 1 minute from now
+$_SESSION['destroy_time'] = time() + 60;
+
+// Clear session variables
 $_SESSION = array();
 
-
-session_destroy();
-
-
-if (isset($_COOKIE['username'])) {
-    setcookie('username', '', time() - 3600, "/"); 
-}
-if (isset($_COOKIE['password'])) {
-    setcookie('password', '', time() - 3600, "/"); 
+// Clear remember me cookie if it exists
+if (isset($_COOKIE['remember_me'])) {
+    setcookie('remember_me', '', time() - 3600, "/");
 }
 
 // Redirect to login page after logout
